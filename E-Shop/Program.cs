beb2bb6595501a0c3461fe0f5cee0fbc5b4ebba0
WizMe-M <enderman.2016@mail.ru>
2021-05-26 15:23:34 +0300

    Добавьте файлы проекта.

---
+++ E-Shop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace E_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Электронный магазин");
+        }
+    }
+}
